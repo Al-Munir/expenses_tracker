ec437a8c2d582732f1a2eba9a766e801e6deaf06
almunir <almunirmarwaan@gmail.com>
2023-03-03 19:43:19 +0000

    initial commit

---
+++ ExpensesTracker.py
@@ -0,0 +1,69 @@
+import csv
+import gspread
+import time
+
+
+MONTH = input('Enter the month and year you wish to extract in MM-YYYY format: ')
+MNTH = input('Enter first three letters of Month in CAPS: ')
+
+file = f'{MONTH}.csv'
+
+transactions = []
+
+def hsbcFin(file):
+
+    with open(file, mode ='r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            date = row[0]
+            description = str(row[1]).lower()
+            description2 = description.upper()
+            amount1 = row[2]
+            amount2 = amount1.replace(',','')
+            amount = float(amount2)
+            category = 'Other'
+
+            if 'just eat co uk ltd' in description:
+                category = 'WAGES'
+
+            elif 'eat' in description:
+                category = 'FOOD'  
+
+            elif 'ee limited' in description:
+                category = 'MOBILE'  
+
+            elif 'gift' in description:
+                category = 'GIFT' 
+
+            elif 'amz' in description:
+                category = 'PURCHASES' 
+
+            elif 'tower hamlets' in description:
+                category = 'RENT' 
+
+            elif 'tfl' in description:
+                category = 'TRAVEL' 
+
+            elif 'cash' in description:
+                category = 'CASH WITHDRAWAL' 
+
+            elif 'minted' in description:
+                category = 'GOLD INVESTMENT' 
+
+            transaction = ((date, description2, amount, category))
+            print(transaction)
+            transactions.append(transaction)
+        return transactions
+
+sa = gspread.service_account()
+sh = sa.open('Expenses Tracker')
+
+wks = sh.worksheet(f'{MNTH}')
+
+rows = hsbcFin(file)
+
+for row in rows:
+    wks.insert_row([row[0], row[1], row[3], row[2]],2)
+    time.sleep(2)
+
+
